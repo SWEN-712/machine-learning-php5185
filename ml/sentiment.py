c1e3f89e0fedbbc8e360e2ac1d3eec2a9957330a
php5185 <php5185@rit.edu>
2020-02-18 22:03:58 -0500

    from tweet to Jsom

--- ml/sentiment.py
+++ ml/sentiment.py
@@ -7,17 +7,23 @@ import extractTweets
 # subscription_key = "8df7fd266c994b2d96cae75ecc5c1c6d"
 # endpoint = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment/"
 
-count =0;
+count =1;
 #format tweet to json here
 info = extractTweets.tweets()
 
 tweet = []
+for x in info:
+    id = str(count)
+    each ={
+        "id":id,
+        "text":x
+    }
+    count +=1
+    tweet.append(each)
 
-each ={
-    
+document = {
+    "documents": tweet
 }
-
-
 
 # headers = {"Ocp-Apim-Subscription-Key": subscription_key}
 # response = requests.post(language_api_url, headers=headers, json=documents)
